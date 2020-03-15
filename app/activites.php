b0c744b796bb930582020ebe049b0eca950d08bb
kmike-wachira <mwkariuki2e@gmail.com>
2020-03-15 11:30:26 +0300

    created models for the project

---
+++ app/activites.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class activites extends Model
+{
+    //
+}
---
+++ database/migrations/2020_03_15_075842_create_event_venues_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventVenuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_venues', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('venue_name');
+            $table->string('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_venues');
+    }
+}
