95c3824ab6b2baa1a4de86d59b79d66776550635
kmike-wachira <mwkariuki2e@gmail.com>
2020-03-16 09:05:31 +0300

    Adding margin to new layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('body')
-<div class="container">
+<div class="container pb-4 mtop">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
